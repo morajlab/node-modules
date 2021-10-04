00a8b594c95a95e83a1cf7e3034b4129b1222345
Morteza Jamali <mortezajamali4241@gmail.com>
2021-10-04 15:36:13 +0000

    chore: write tests for 'utils/style'

--- ui-dev/utils/style/style.spec.tsx
+++ ui-dev/utils/style/style.spec.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { BasicStyle } from './style.composition';
+import React from "react";
+import { render } from "@testing-library/react";
+import { style, getRenderedComponentStyles } from "./style";
 
-it('should render with the correct text', () => {
-  const { getByText } = render(<BasicStyle />);
-  const rendered = getByText('hello from Style');
-  expect(rendered).toBeTruthy();
+describe("Style utility tests", () => {
+  it("Should render with the correct style", () => {
+    const styles = {
+      backgroundColor: "rgb(0, 233, 15)",
+      color: "rgb(255, 255, 255)",
+      fontSize: "12px",
+    };
+
+    const StyledComponent = () => <div {...style(styles)} />;
+
+    render(<StyledComponent />);
+
+    const computedStyles = getRenderedComponentStyles(StyledComponent());
+
+    for (const [key, value] of Object.entries(styles)) {
+      expect(computedStyles[key]).toBe(value);
+    }
+  });
 });
--- ui-dev/utils/style/style.ts
+++ ui-dev/utils/style/style.ts
@@ -1,4 +1,15 @@
+export * from "glamor";
 import { css } from "glamor";
 import type { IStyleFunction } from "./style.types";
 
 export const style: IStyleFunction = (props) => css(props);
+
+export const getRenderedComponentStyles = (component: JSX.Element) => {
+  for (const [key, _value] of Object.entries(component.props)) {
+    if (key.includes("data-css")) {
+      const element = document.querySelectorAll(`[${key}]`)[0];
+
+      return window.getComputedStyle(element);
+    }
+  }
+};
